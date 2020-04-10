c98f39eba8e5f5019fb42bb305f18d3045ff96ef
ReiWing6400 <kaze.no.kurai@gmail.com>
2020-04-10 23:57:52 +0300

    fix shadow names

--- logistic.py
+++ logistic.py
@@ -41,14 +41,11 @@ bias = np.ones(n_pts)
 top_region = np.array([np.random.normal(10, 2, n_pts), np.random.normal(12, 2, n_pts), bias]).T
 bottom_region = np.array([np.random.normal(5, 2, n_pts), np.random.normal(6, 2, n_pts), bias]).T
 all_points = np.vstack((top_region, bottom_region))
-line_parameters = np.matrix([np.zeros(3)]).T
-# w1x1 + w2x2 + b = 0
-# x1 = np.array([bottom_region[:, 0].min(), top_region[:, 0].max()])
-# x2 = - b / w2 + x1 * (- w1 / w2)
-y = np.array([np.zeros(n_pts), np.ones(n_pts)]).reshape(n_pts*2, 1)
+zero_line_parameters = np.matrix([np.zeros(3)]).T
+y_label = np.array([np.zeros(n_pts), np.ones(n_pts)]).reshape(n_pts * 2, 1)
 
 _, ax = plt.subplots(figsize=(5, 5))
 ax.scatter(top_region[:, 0], top_region[:, 1], color='r')
 ax.scatter(bottom_region[:, 0], bottom_region[:, 1], color='b')
-gradient_descent(line_parameters, all_points, y, 0.01)
+gradient_descent(zero_line_parameters, all_points, y_label, 0.01)
 plt.show()
